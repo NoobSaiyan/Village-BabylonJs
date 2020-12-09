9cd280eb511d644ac8281aaaed33f44c47f5fef9
NoobSaiyan <ms19961998@gmail.com>
2020-12-09 15:32:21 +0530

    wrapping texture around box

--- public/Main.js
+++ public/Main.js
@@ -25,10 +25,22 @@ const createScene = () => {
 }
 
 const createBox = () => {
+  //texture
   const boxMat = new BABYLON.StandardMaterial('boxMat')
-  boxMat.diffuseTexture = new BABYLON.Texture('./texture/wall.jpg')
+  boxMat.diffuseTexture = new BABYLON.Texture('./texture/cubehouse.png')
 
-  const box = BABYLON.MeshBuilder.CreateBox('box', {})
+  //options parameter to set different images on each side
+  const faceUV = []
+  faceUV[0] = new BABYLON.Vector4(0.5, 0.0, 0.75, 1.0) //rear face
+  faceUV[1] = new BABYLON.Vector4(0.0, 0.0, 0.25, 1.0) //front face
+  faceUV[2] = new BABYLON.Vector4(0.25, 0, 0.5, 1.0) //right side
+  faceUV[3] = new BABYLON.Vector4(0.75, 0, 1.0, 1.0) //left side
+  // top 4 and bottom 5 not seen so not set
+
+  const box = BABYLON.MeshBuilder.CreateBox('box', {
+    faceUV: faceUV,
+    wrap: true,
+  })
   box.position.y = 0.5
   box.material = boxMat
 }
